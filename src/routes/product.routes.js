f8eeb31c1ac850ed159fa1176b82d78cca619d4c
gonza1020 <gonzalozoloaga44@gmail.com>
2022-04-07 10:40:54 -0300

    Cambiando el puerto a 4000 Cambio nombre de product controller

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,14 +1,14 @@
 import {Router} from 'express'
 import ProductController from '../controllers/prod.controller.js'
 
-const controller = new ProductController()
+const productController = new ProductController()
 
 const router = Router(); 
-router.get('/products',controller.getAll);
-router.get('/products/:id',controller.getOneProduct);
-router.post('/products',controller.createProduct)
-router.delete('/products/:id',controller.deleteProduct)
-router.put('/products/:id',controller.updateProduct)
+router.get('/products',productController.getAll);
+router.get('/products/:id',productController.getOneProduct);
+router.post('/products',productController.createProduct)
+router.delete('/products/:id',productController.deleteProduct)
+router.put('/products/:id',productController.updateProduct)
 
  export default router
 
